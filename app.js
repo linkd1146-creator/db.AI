d2bbfdf993179fc69dc6a777cc11daa29529b1bf
linkd1146-creator <linkd1146@gmail.com>
2026-05-12 00:31:50 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, si
 from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "ISI_API_KEY",
-  authDomain: "ISI_DOMAIN",
-  projectId: "ISI_PROJECT_ID",
+  apiKey: "AIzaSyBioqM7NYa4FDvQ3zUISGt_YS_b-TThUNk",
+  authDomain: "db-ai-b73e5.firebaseapp.com",
+  projectId: "db-ai-b73e5",
+  storageBucket: "db-ai-b73e5.firebasestorage.app",
+  messagingSenderId: "617783948329",
+  appId: "1:617783948329:web:18d9942e8c3b73858dd838",
+  measurementId: "G-ZZGV797JYQ"
 };
 
 const app = initializeApp(firebaseConfig);
