00a5a4c8461dd28b3b361d0b172ca3275ee37cef
linkd1146-creator <linkd1146@gmail.com>
2026-05-12 06:34:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBioqM7NYa4FDvQ3zUISGt_YS_b-TThUNk",
-  authDomain: "db-ai-b73e5.firebaseapp.com",
-  projectId: "db-ai-b73e5",
-  storageBucket: "db-ai-b73e5.firebasestorage.app",
-  messagingSenderId: "617783948329",
-  appId: "1:617783948329:web:18d9942e8c3b73858dd838",
-  measurementId: "G-ZZGV797JYQ"
-};
+    apiKey: "AIzaSyBioqM7NYa4FDvQ3zUISGt_YS_b-TThUNk",
+    authDomain: "db-ai-b73e5.firebaseapp.com",
+    projectId: "db-ai-b73e5",
+    storageBucket: "db-ai-b73e5.firebasestorage.app",
+    messagingSenderId: "617783948329",
+    appId: "1:617783948329:web:18d9942e8c3b73858dd838",
+    measurementId: "G-ZZGV797JYQ"
+  };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
